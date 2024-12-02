833180eed8f7d7d1791d0010e956bb19554bc27b
Evan Wildenhain <evan.wildenhain@gmail.com>
2024-12-01 22:06:16 -0500

    day 1

---
+++ src/aoc2024/aoc2024.clj
@@ -0,0 +1,12 @@
+(ns aoc2024.aoc2024
+  "advent of code 2024")
+
+(defn exec
+  "Invoke me with clojure -X aoc2024.aoc2024/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m aoc2024.aoc2024"
+  [& args]
+  (println "-main with" args))
---
+++ src/aoc2024/day01.clj
@@ -0,0 +1,38 @@
+(ns aoc2024.day01
+  "advent of code 2024"
+  (:require
+   [clojure.string :as str]))
+
+(def day01-input
+  (slurp "input/01.txt"))
+
+(def num-lists
+  (->> (str/split-lines day01-input)
+       (reduce (fn [[accx accy] line]
+                 (let [[x y] (str/split line #" +")
+                       accx' (conj accx (Integer/parseInt x))
+                       accy' (conj accy (Integer/parseInt y))]
+                   [accx' accy'])) [])))
+
+(def left (get num-lists 0))
+(def right (get num-lists 1))
+
+(comment "day 1 part 1")
+(comment
+  (require '[clojure.string :as str])
+  (->> num-lists
+       (map sort)
+       (apply (fn [a b] (map vector a b)))
+       (map #(- (get % 0) (get % 1)))
+       (map abs)
+       (reduce +))
+  :rcf)
+
+(comment "day 1 part 2")
+(comment
+  (def right-counts
+    (frequencies right))
+  (->> left
+       (map (fn [elem] (* elem (get right-counts elem 0))))
+       (reduce +))
+  :rcf)
