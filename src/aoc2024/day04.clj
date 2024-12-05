ba4be63430ef7b67ab638c21df1c49b02f168321
Evan Wildenhain <evan.wildenhain@gmail.com>
2024-12-04 20:10:34 -0500

    day 4 part 2 not working yet

--- src/aoc2024/day04.clj
+++ src/aoc2024/day04.clj
@@ -56,3 +56,65 @@
 (def part1
   (count (filter (partial apply check-with-pattern) candidates)))
 
+(def part2-corner-patterns
+  [[:tl :bl]
+   [:tl :tr]
+   [:tr :br]
+   [:bl :br]])
+
+(defn to-abstract-part2 [corner-pat]
+  (map (fn [corner] (case corner
+                      :tl [:asc :asc]
+                      :bl [:desc :asc]
+                      :tr [:asc :desc]
+                      :br [:desc :desc])) corner-pat))
+
+(comment
+  (to-abstract-part2 [:tl :bl]))
+
+(defn to-concrete-part2 [abstpat]
+  (case abstpat
+    :asc [0 1 2]
+    :desc [0 -1 -2]))
+
+(comment
+  (map (fn [abstpats] (map to-concrete-part2 abstpats))
+       (to-abstract-part2 [:tl :bl])))
+
+(defn zip [a b]
+  (map vector a b))
+
+(def coord-offsets
+  (->> part2-corner-patterns
+       (map to-abstract-part2)
+       (map #(map (fn [abstpats] (map to-concrete-part2 abstpats)) %))
+       (map (fn [ls] (map #(apply zip %) ls)))
+       ;; (map (fn [[roffs coffs]] (map vector roffs coffs)))
+       ;; (map (fn [coordpats] (map (fn [rpat cpat] (map vector rpat cpat)) coordpats)))
+       ))
+(def candidates-part2
+  (for [offsets coord-offsets
+        r (range (count twodarr))
+        c (range (count (first twodarr)))]
+    (map (fn [offsets] (map (fn [[roff coff]] [(+ r roff) (+ c coff)]) offsets)) offsets)))
+
+(comment
+  (nth candidates-part2 100))
+
+(defn extract-candidate-part2 [candidate]
+  (map (fn [coords]
+         (map #(get-in twodarr %) coords)) candidate))
+
+(comment
+  (extract-candidate-part2 (nth candidates-part2 100)))
+
+(defn check-candidate-part2 [candidate]
+  (->> (extract-candidate-part2 candidate)
+       (every? (partial = ["M" "A" "S"]))))
+
+(comment (check-candidate-part2 (nth candidates-part2 100)))
+
+(comment (map extract-candidate-part2 candidates-part2))
+
+(def part2 (count (filter check-candidate-part2 candidates-part2)))
+
