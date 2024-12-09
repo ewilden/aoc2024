6600f047a3e8c3909a453250ca204c3dfe5b2e50
Evan Wildenhain <evan.wildenhain@gmail.com>
2024-12-09 07:34:43 -0500

    fmt

--- src/aoc2024/day08.clj
+++ src/aoc2024/day08.clj
@@ -18,18 +18,16 @@
 (def num-cols (count (first grid)))
 
 (def grid-map
-  (->> grid 
+  (->> grid
        (map-indexed (fn [r row] (map-indexed (fn [c item] [[r c] item]) row)))
        (apply concat)
        (filter #(not= \. (second %)))
-       (into {})
-  ))
+       (into {})))
 
 (def by-freq (update-vals (group-by second grid-map)
-                          #(map first %)
-                  ))
+                          #(map first %)))
 
-(defn antinodes [chr] 
+(defn antinodes [chr]
   (let [locs (by-freq chr)
         antinode-locs (remove nil? (for [a locs
                                          b locs]
@@ -40,11 +38,11 @@
                                          [aside bside]))))]
     (->> antinode-locs
          (apply concat)
-         (filter (fn [[r c]] (and 
-                               (>= r 0)
-                               (< r num-rows)
-                               (>= c 0)
-                               (< c num-cols)))))))
+         (filter (fn [[r c]] (and
+                              (>= r 0)
+                              (< r num-rows)
+                              (>= c 0)
+                              (< c num-cols)))))))
 
 (def part1
   (->> (map antinodes (keys by-freq))
@@ -55,20 +53,20 @@
 (defn numer [n] (if (ratio? n) (numerator n) n))
 (defn denom [n] (if (ratio? n) (denominator n) 1))
 
-(comment r = mc + b 
+(comment r = mc + b
          b = r - mc
          c = (r - b) / m)
 
 (def maxcoord (- (count grid) 1))
 
-(defn points-in-line [ [r1 c1] [r2 c2]]
+(defn points-in-line [[r1 c1] [r2 c2]]
   (let [m (/ (- r2 r1) (- c2 c1))
         ;; bigger (max (numer rat) (denom rat))
         ;; smaller (max (numer rat) (denom rat))
         b (- r1  (* m c1))
-        pts-left ( ->> (iterate (fn [[r c]] [(- r (numer m)) (- c (denom m))]) [r1 c1]) 
-                       (take-while (fn [dims] (every? #(and (>= % 0) (<= % maxcoord)) dims))))
-        pts-right ( ->> (iterate (fn [[r c]] [(+ r (numer m)) (+ c (denom m))]) [r1 c1]) 
+        pts-left (->> (iterate (fn [[r c]] [(- r (numer m)) (- c (denom m))]) [r1 c1])
+                      (take-while (fn [dims] (every? #(and (>= % 0) (<= % maxcoord)) dims))))
+        pts-right (->> (iterate (fn [[r c]] [(+ r (numer m)) (+ c (denom m))]) [r1 c1])
                        (take-while (fn [dims] (every? #(and (>= % 0) (<= % maxcoord)) dims))))
         ;; r_when_c_0 b 
         ;; r_when_c_maxcoord (+ b (* m maxcoord))
@@ -87,9 +85,7 @@
       ;; (print [r_below r_above c_below c_above])
       ;; (min (+ 1 (long (floor r_below)) (long (floor r_above)))
       ;;      (+ 1 (long (floor c_below)) (long (floor c_above))))
-    )
-  )
-
+    ))
 (def part2
   (->> (map second by-freq)
        (map (fn [locs] (for [a locs b locs] (when (not= a b) (points-in-line a b)))))
@@ -97,6 +93,5 @@
        (apply concat)
        (apply concat)
        (into #{})
-       (count)
-       ))
+       (count)))
 
