971c0ddfe6af234f5dd719b72a8a0a3dc9c2ad9d
Evan Wildenhain <evan.wildenhain@gmail.com>
2024-12-20 23:48:28 -0800

    day 19 part 1

---
+++ src/aoc2024/day19.clj
@@ -0,0 +1,69 @@
+(ns aoc2024.day19
+  "day 19"
+  (:require
+   [clojure.string :as str]))
+
+(require '[clojure.core.match :refer [match]])
+
+(def day19-input
+  (str/trim (slurp "input/19.txt")))
+
+(def sample "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb")
+
+(def use-sample false)
+
+(def input (if use-sample sample day19-input))
+
+(def patterns
+  (let [[pats _] (str/split input #"\n\n")
+        pats (str/split pats #", ")]
+    pats))
+
+(def designs
+  (let [[_ designs] (str/split input #"\n\n")]
+    (str/split-lines designs)))
+
+(defn build-trie [seed & kvs]
+  (reduce
+   (fn [trie [k v]]
+     (assoc-in trie (concat k [:val]) v))
+   seed
+   (partition 2 kvs)))
+
+(defn prefix-match [target trie]
+  (when (seq target)
+    (when-let [node (trie (first target))]
+      (let [h (:val node)
+            tl (prefix-match (rest target) node)]
+        (if h
+          (cons h tl)
+          tl)))))
+
+(def trie (apply build-trie {} (interleave patterns patterns)))
+
+(defn munch-prefix-of-design [design]
+  (->> (prefix-match design trie)
+       (map #(subs design (count %)))))
+
+(defn design-is-possible [designs]
+  (if (some (partial = "") designs) true
+      (let [designs (into #{} (mapcat munch-prefix-of-design designs))]
+        (if (empty? designs)
+          false
+          (recur designs)))))
+
+(def part1
+  (->> designs
+       (filter #(design-is-possible #{%}))
+       (count)))
+
+(println "part1:" part1)
